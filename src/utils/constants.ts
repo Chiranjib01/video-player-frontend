790de048a6f1780a140a0f53c19ce022391c9b6c
chiranjib <chiranjibnath21313@gmail.com>
2023-07-19 01:08:15 +0530

    page title

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = "Vidster";
-// export const API_URL = "https://video-player-00.vercel.app";
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://video-player-00.vercel.app";
+// export const API_URL = "http://localhost:5000";
